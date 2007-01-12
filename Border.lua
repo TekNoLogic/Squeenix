57105b19dcf7c1c955d63a17966c2ceb79b4e98a
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-01-12 09:53:02 +0000

    Squeenix - Borders are back - Tweaked some button placement
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/Squeenix@165 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- Border.lua
+++ Border.lua
@@ -1,13 +1,11 @@
-local L = AceLibrary("AceLocale-2.2"):new("SqueenixBorder")
-L:RegisterTranslations("enUS", function() return {
-	["Border style"] = true,
-	["Change the minimap border style"] = true,
-} end)
-
-L:RegisterTranslations("koKR", function() return {
+local L = GetLocale == "koKR" and {
 	["Border style"] = "외곽선",
 	["Change the minimap border style"] = "미니맵의 외곽선의 종류를 변경합니다",
-} end)
+} or {
+	["Border style"] = true,
+	["Change the minimap border style"] = true,
+}
+
 
 local borders = {
 	["Rounded"] = {bgFile = "Interface\\Tooltips\\UI-Tooltip-Background", tile = true, tileSize = 16, edgeSize = 16,
@@ -20,41 +18,41 @@ local borders = {
 		edgeFile = "", edgeSize = 0, insets = {left = 3, right = 3, top = 3, bottom = 3}},
 }
 
-local border = Squeenix:NewModule("Border")
+
+local border = Squeenix:NewModule("Squeenix-Border")
 
 
-function border:OnInitialize()
-	local self = self
-	self.db = Squeenix:AcquireDBNamespace("Border")
-	Squeenix:RegisterDefaults("Border", "profile", {style = "Square"})
+function border:Initialize()
+--~ 	self.db = Squeenix:AcquireDBNamespace("Border")
+--~ 	Squeenix:RegisterDefaults("Border", "profile", {style = "Square"})
 
-	local borderList = {}
-	for k in pairs(borders) do table.insert(borderList, k) end
+--~ 	local borderList = {}
+--~ 	for k in pairs(borders) do table.insert(borderList, k) end
 
-	Squeenix.Options.args.border = {
-		name = L["Border style"],
-		type = "text",
-		desc = L["Change the minimap border style"],
-		handler = self,
-		get = function() return self.db.profile.style end,
-		set = "SetBorder",
-		validate = borderList,
-	}
+--~ 	Squeenix.Options.args.border = {
+--~ 		name = L["Border style"],
+--~ 		type = "text",
+--~ 		desc = L["Change the minimap border style"],
+--~ 		handler = self,
+--~ 		get = function() return self.db.profile.style end,
+--~ 		set = "SetBorder",
+--~ 		validate = borderList,
+--~ 	}
 
 	self.frame = CreateFrame("Button", nil, Minimap)
 	self.frame:SetPoint("TOPLEFT","Minimap",-5,5)
 	self.frame:SetPoint("BOTTOMRIGHT","Minimap",5,-5)
 
 	self:SetBorder()
-	self.frame:SetBackdropColor(0,0,0,1)
 	self.frame:SetFrameStrata("BACKGROUND")
 	self.frame:SetFrameLevel(1)
 end
 
 
 function border:SetBorder(v)
-	if v then self.db.profile.style = v end
-	self.frame:SetBackdrop(borders[self.db.profile.style])
+--~ 	if v then self.db.profile.style = v end
+--~ 	self.frame:SetBackdrop(borders[self.db.profile.style])
+	self.frame:SetBackdrop(borders["Rounded"])
 	self.frame:SetBackdropColor(0,0,0,1)
 end
 
