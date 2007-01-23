6ab353581ffc4b5aee4e3f01733e39632cb2b980
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-01-23 12:36:36 +0000

    Squeenix - Scrollzoom module should work now
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/Squeenix@211 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- Scrollzoom.lua
+++ Scrollzoom.lua
@@ -1,37 +1,34 @@
-local L = AceLibrary("AceLocale-2.2"):new("SqueenixScrollZoom")
-L:RegisterTranslations("enUS", function() return {
-	["Mousewheel zoom"] = true,
-	["Use mousewheel to control minimap zoom"] = true,
-} end)
-
-L:RegisterTranslations("koKR", function() return {
+local L = GetLocale() == "koKR" and {
 	["Mousewheel zoom"] = "마우스휠 사용",
 	["Use mousewheel to control minimap zoom"] = "미니맵을 마우스휠을 사용해서 확대/축소할 수 있습니다",
-} end)
+} or {
+	["Mousewheel zoom"] = true,
+	["Use mousewheel to control minimap zoom"] = true,
+}
 
 local zoom = Squeenix:NewModule("Scroll Zoom")
 
-function zoom:OnInitialize()
-	self.db = Squeenix:AcquireDBNamespace("Scroll Zoom")
-	Squeenix:RegisterDefaults("Scroll Zoom", "profile", {scroll = true})
-	Squeenix.Options.args.scroll = {
-		name = L["Mousewheel zoom"],
-		type = "toggle",
-		desc = L["Use mousewheel to control minimap zoom"],
-		get = function() return zoom.db.profile.scroll end,
-		set = function(v) zoom.db.profile.scroll = v end,
-	}
+function zoom:Initialize()
+--~ 	self.db = Squeenix:AcquireDBNamespace("Scroll Zoom")
+--~ 	Squeenix:RegisterDefaults("Scroll Zoom", "profile", {scroll = true})
+--~ 	Squeenix.Options.args.scroll = {
+--~ 		name = L["Mousewheel zoom"],
+--~ 		type = "toggle",
+--~ 		desc = L["Use mousewheel to control minimap zoom"],
+--~ 		get = function() return zoom.db.profile.scroll end,
+--~ 		set = function(v) zoom.db.profile.scroll = v end,
+--~ 	}
 
-	local MinimapZoom = CreateFrame("Frame", nil, Minimap)
-	MinimapZoom:SetFrameStrata("LOW")
-	MinimapZoom:EnableMouse(false)
-	MinimapZoom:SetPoint("TOPLEFT", Minimap, "TOPLEFT")
-	MinimapZoom:SetPoint("BOTTOMRIGHT", Minimap, "BOTTOMRIGHT")
-	MinimapZoom:EnableMouseWheel(true)
-	MinimapZoom:SetScript("OnMouseWheel", function()
-		if not zoom.db.profile.scroll then return end
-		if arg1 > 0 then MinimapZoomIn:Click()
-		elseif arg1 < 0 then MinimapZoomOut:Click() end
+	local f = CreateFrame("Frame", nil, Minimap)
+	f:SetFrameStrata("LOW")
+	f:EnableMouse(false)
+	f:SetPoint("TOPLEFT", Minimap, "TOPLEFT")
+	f:SetPoint("BOTTOMRIGHT", Minimap, "BOTTOMRIGHT")
+	f:EnableMouseWheel(true)
+	f:SetScript("OnMouseWheel", function(frame, delta)
+--~ 		if not zoom.db.profile.scroll then return end
+		if delta > 0 then MinimapZoomIn:Click()
+		elseif delta < 0 then MinimapZoomOut:Click() end
 	end)
 end
 
