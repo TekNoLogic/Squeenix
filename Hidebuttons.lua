5c2e6718f6bdef3393e0d85d7a66b44d5bb08e2c
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-03-14 17:48:22 -0600

    Add mail frame to button hider

--- Hidebuttons.lua
+++ Hidebuttons.lua
@@ -16,8 +16,16 @@ local GAP = 8
 --      Initialization      --
 ------------------------------
 
+local mailshow = MiniMapMailFrame.Show
 function Squeenix:HideButtons()
 	if self.db.hideMinimapZoom then MinimapZoomIn:Hide(); MinimapZoomOut:Hide() else MinimapZoomIn:Show(); MinimapZoomOut:Show() end
+	if self.db.hideMiniMapMailFrame then
+		MiniMapMailFrame.Show = MiniMapMailFrame.Hide
+		MiniMapMailFrame:Hide()
+	else
+		MiniMapMailFrame.Show = mailshow
+		if HasNewMail() then MiniMapMailFrame:Show() end
+	end
 	for name in pairs(frames) do
 		if self.db["hide"..name] then _G[name]:Hide() else _G[name]:Show() end
 	end
@@ -43,7 +51,12 @@ frame:SetScript("OnShow", function(frame)
 	zoom:SetChecked(not Squeenix.db.hideMinimapZoom)
 
 
-	local anchor = zoom
+	local mail = tekcheck.new(frame, nil, "Show new mail indicator", "TOPLEFT", zoom, "BOTTOMLEFT", 0, -GAP)
+	mail:SetScript("OnClick", function(self) checksound(self); Squeenix.db.hideMiniMapMailFrame = not Squeenix.db.hideMiniMapMailFrame; Squeenix:HideButtons() end)
+	mail:SetChecked(not Squeenix.db.hideMiniMapMailFrame)
+
+
+	local anchor = mail
 	for name,desc in pairs(frames) do
 		local check = tekcheck.new(frame, nil, "Show "..desc, "TOPLEFT", anchor, "BOTTOMLEFT", 0, -GAP)
 		check:SetScript("OnClick", function(self) checksound(self); Squeenix.db["hide"..name] = not Squeenix.db["hide"..name]; Squeenix:HideButtons() end)
